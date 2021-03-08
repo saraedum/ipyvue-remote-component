ef7bdc0b058ac76c956304ce01522a6facd69b5a
Julian Rüth <julian.rueth@fsfe.org>
2021-03-08 22:59:56 +0100

    fix comment

--- js/lib/activate.js
+++ js/lib/activate.js
@@ -2,6 +2,6 @@ import { Vue } from "jupyter-vue";
 import RemoteComponent from "vue-remote-component";
 
 export function activate(app, widget) {
-  // Register <vue-component/> as a valid tag with Vue.js.
+  // Register <remote-component/> as a tag with Vue.js.
   Vue.component("remote-component", RemoteComponent);
 }
